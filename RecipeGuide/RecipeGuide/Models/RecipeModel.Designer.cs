ac5d5ba778fc894788dc282779d36d1f9ffe0cd2
mwerner340 <mikewerner@att.net>
2014-07-30 12:07:19 -0400

    string rename

--- RecipeGuide/RecipeGuide/Models/RecipeModel.Designer.cs
+++ RecipeGuide/RecipeGuide/Models/RecipeModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Stephanie\Documents\GitHub\TeamProject1\ThreeDayProject\RecipeGuide\RecipeGuide\Models\RecipeModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Michelle\Documents\GitHub\ThreeDayProject\RecipeGuide\RecipeGuide\Models\RecipeModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
